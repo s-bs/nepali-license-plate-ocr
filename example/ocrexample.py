127e5d9862fe4921a6256e6c454bb628f58b31a4
hope <sumubam191@gmail.com>
2024-03-19 16:39:55 +0545

    second re

--- example/ocrexample.py
+++ example/ocrexample.py
@@ -6,31 +6,6 @@ import cv2
 from PIL import Image
 
 
-
-# if __name__ == '__main__':
-#     image_path =cv2.imread('D:/Python/nepali-licence-plate/resources/images/dataset/5.png')
-#     object_ocr = ocr_easyocr.EasyOCR()
-#     result = object_ocr.extract_text(image_path)
-    
-# if __name__ == '__main__':
-#     # Open the image
-#     image_path = Image.open("D:/Python/nepali-licence-plate/resources/images/dataset/2.jpg")
-#     object_ocr = ocr_easyocr.EasyOCR()
-#     result = object_ocr.resize_image(image_path)
-
-
-# if __name__ == '__main__':
-#     # Open the image
-#     image_path = Image.open("D:/Python/nepali-licence-plate/resources/images/dataset/2.jpg")
-#     object_ocr = ocr_easyocr.EasyOCR()
-#     result = object_ocr.image_contrast(image_path)
-    
-# if __name__ == '__main__':
-#     # Open the image
-#     image_path =cv2.imread('D:/Python/nepali-licence-plate/resources/images/dataset/5.png')
-#     object_ocr = ocr_easyocr.EasyOCR()
-#     result = object_ocr.image_binary(image_path)
-
 if __name__ == '__main__':
     # Open the image
     object_ocr = ocr_easyocr.EasyOCR()
